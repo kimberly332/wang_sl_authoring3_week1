2da1aa9b26e3ef9b32977f8c4ff42b1298999a35
kimberly332 <kimberly.wang1010@gmail.com>
2020-10-06 19:39:34 -0400

    adding mixins to sass

--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,25 @@
-module.exports = {
-    name: "Trevor",
-    role: "Prof",
-    nickname: "Tervor"
+export default {
+    Justin: {
+        name: "Justin",
+        role: "Coordinator",
+        nickname: "I don't know"
+    },
+
+    John: {
+        name: "John",
+        role: "super chill prof",
+        nickname: "just john"
+    },
+
+    Joe: {
+        name: "Joe",
+        role: "Prof",
+        nickname: "Teddy Bear"
+    },
+
+    Pan: {
+        name: "Pan",
+        role: "Code Ninja",
+        nickname: "Spider Pan"
+    }
 }
