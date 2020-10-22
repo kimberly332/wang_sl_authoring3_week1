5516dc0a4c7947e9909ef56667a14167f6bbb9af
kimberly332 <kimberly.wang1010@gmail.com>
2020-10-22 15:51:16 -0400

    spliting js into modules

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-(() => {
-    // start with a Fetch all
-    fetch('./DataSet.json')
-        .then(res => res.json()) // parse the JSON (translate) back to plain JS
-        .then(data => {
-            // this is our data (DataSet.json)
-            // converted to a plain JavaScript object
-            handleDataSet(data);
-        })
-    .catch((error) => console.log(error));
+import {fetchData} from "./modules/DataMiner.js";
 
+(() => {
     // select our user elements and load the content
     function handleDataSet(data) {
         let userSection = document.querySelector('.user-section'),
@@ -20,10 +12,10 @@
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector(".user").children;
 
-             currentUserText[1].src = `images/${data[user].avatar}.jpg`;
-             currentUserText[1].textContent = data[user].name;
-             currentUserText[2].textContent = data[user].role;
-             currentUserText[3].textContent = data[user].nickname;
+             currentUserText[1].src = `images/${data[user].avatar}`;
+             currentUserText[2].textContent = data[user].name;
+             currentUserText[3].textContent = data[user].role;
+             currentUserText[4].textContent = data[user].nickname;
  
             // add this new user to the view
              userSection.appendChild(currentUser);
@@ -31,4 +23,6 @@
 
          console.log(data);
     }
+
+    fetchData('./DataSet.json').then(data => handleDataSet(data)).catch(err => console.log(err));
 })();
---
+++ js/modules/DataMiner.js
@@ -0,0 +1,17 @@
+async function fetchData(datasource) {
+    let resource = await fetch(datasource).then(response => {
+        // bang operator - means "does not equal" (or a falsy value)
+        if(response.status !== 200) {
+            throw new Error(`Danger will Robinson! Here there be montsers! Error ${response.status}`);
+        }
+        
+        return response;
+    })
+
+    // if we get success, then we can return back our resource after parse it into plain JS
+    let dataset = await resource.json();
+
+    return dataset;
+}
+
+export {fetchData};
