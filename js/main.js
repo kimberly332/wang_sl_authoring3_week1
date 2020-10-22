64cff76e99d115c549745759aeadb70971a7e236
kimberly332 <kimberly.wang1010@gmail.com>
2020-10-22 13:03:54 -0400

    adding fetch to this project

--- js/main.js
+++ js/main.js
@@ -1,67 +1,34 @@
-// import your packages here
-import Team from "./modules/DataModule.js";
-
 (() => {
-    // stub * just a place for non-component-specific stuff
-    
-    // set up the XMLHTTP object
-    let myReq = new XMLHttpRequest;
-
-    // make sure we can handle whatever data comes back, or any errors
-    myReq.addEventListener("readystatechange", handleRequest);
-
-    // open a request and pass thru the URL of the data that we want
-    myReq.open('GET', '../DataSet.json');
-
-    // actually make the request
-    myReq.send();
-
-    // handleRequest function goes here
-    function handleRequest() {
-        if (myReq.readyState === XMLHttpRequest.DONE) {
-            // check status here and proceed
-            if (myReq.status === 200) {
-                // 200 means done and dusted, ready to go with the dataset!
-                handleDataSet(JSON.parse(myReq.responseText));
-
-            } else {
-                // probably got some kind of error code, so handle that 
-                // a 404, 500 etc... can render appropriate error messages here
-                console.error(`${myReq.status} : something done broke, son`);
-            }
-        } else {
-            // request isn't ready yet, keep waiting...
-            console.log(`Request state: ${myReq.readyState}. Still processing...`);
-        }
-
-    }
-
-    let userSection = document.querySelector(".user-section"),
-        userTemplate = document.querySelector("#profs-template").content; // grab the content inside the template
-
-    debugger;
+    // start with a Fetch all
+    fetch('./DataSet.json')
+        .then(res => res.json()) // parse the JSON (translate) back to plain JS
+        .then(data => {
+            // this is our data (DataSet.json)
+            // converted to a plain JavaScript object
+            handleDataSet(data);
+        })
+    .catch((error) => console.log(error));
 
     // select our user elements and load the content
     function handleDataSet(data) {
+        let userSection = document.querySelector('.user-section'),
+            userTemplate = document.querySelector('#user-template').content;
+
         debugger;
         
-        for (let user in data) {
-
-            debugger;
-           
-            // make a copy of your template here and then
-            // populate the children (text elements) with
-            // the static data from the Team object    
+        for (let user in data) {  
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector(".user").children;
 
+             currentUserText[1].src = `images/${data[user].avatar}.jpg`;
              currentUserText[1].textContent = data[user].name;
              currentUserText[2].textContent = data[user].role;
              currentUserText[3].textContent = data[user].nickname;
-        
+ 
+            // add this new user to the view
              userSection.appendChild(currentUser);
          }
-    }
 
-    // handleDataSet(Team);
+         console.log(data);
+    }
 })();
